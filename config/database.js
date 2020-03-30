aa24bdf10189722c5c7a63e9dadc898cd9308cbc
katla nithin <nithikatla00@gmail.com>
2020-03-30 17:45:18 +0530

    production ready

--- config/database.js
+++ config/database.js
@@ -1,11 +1,4 @@
-if(process.env.NODE_ENV === 'production'){
-    module.exports = {
-        database: 'mongodb+srv://nithinkatla00:Nithinkatla00@@cluster0-rl7sa.mongodb.net/test?retryWrites=true&w=majority',
-        secret: 'yoursecret'
-    };
-}else{
-    module.exports = {
-        database: 'mongodb://localhost:27017/posts',
-        secret: 'yoursecret'
-    };
-}
+module.exports = {
+    mongoURI: "mongodb+srv://nithinkatla00:katlanithin333@cluster0-rl7sa.mongodb.net/test?retryWrites=true&w=majority",
+    secret: "yoursecret"
+};
