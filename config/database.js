635bb6ec39458b6211ac651f923f49d2f566a83e
katla nithin <nithikatla00@gmail.com>
2020-03-30 17:25:30 +0530

    mevnbasic project

--- config/database.js
+++ config/database.js
@@ -1,4 +1,11 @@
-module.exports = {
-    database: 'mongodb://localhost:27017/posts',
-    secret: 'yoursecret'
+if(process.env.NODE_ENV === 'production'){
+    module.exports = {
+        database: 'mongodb+srv://nithinkatla00:Nithinkatla00@@cluster0-rl7sa.mongodb.net/test?retryWrites=true&w=majority',
+        secret: 'yoursecret'
+    };
+}else{
+    module.exports = {
+        database: 'mongodb://localhost:27017/posts',
+        secret: 'yoursecret'
+    };
 }
