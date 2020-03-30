801b2f73de220734c7c86ef899bd46bd0a93720c
katla nithin <nithikatla00@gmail.com>
2020-03-30 17:56:04 +0530

    make it better

--- config/database.js
+++ config/database.js
@@ -1,4 +1,11 @@
-module.exports = {
-    mongoURI: "mongodb+srv://nithinkatla00:katlanithin333@cluster0-rl7sa.mongodb.net/test?retryWrites=true&w=majority",
-    secret: "yoursecret"
-};
+if(process.env.NODE_ENV === 'production'){
+    module.exports = {
+        mongoURI: "mongodb+srv://nithinkatla00:katlanithin333@cluster0-rl7sa.mongodb.net/test?retryWrites=true&w=majority",
+        secret: 'yoursecret'
+    };
+}else{
+    module.exports = {
+        database: 'mongodb://localhost:27017/posts',
+        secret: 'yoursecret'
+    };
+}
